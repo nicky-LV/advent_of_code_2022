fb3ca79a3def54f5d9af6fb6f872b4e30b02b0b5
nicky-LV <nickystnkvs@outlook.com>
2022-12-13 19:55:17 +0000

    day 7

---
+++ 7/b.py
@@ -0,0 +1,66 @@
+def solution():
+    current_dir = "/"
+    dir_hierarchy = {'/': []}
+    dir_sizes = {'/': 0}
+
+    with open('./input.txt', 'r') as f:
+        for line in f.readlines():
+            line = line.strip('\n')
+
+            # command
+            if line.split(" ")[0] == "$":
+                cmd = line.split(" ")[1]
+
+                # cd into dir
+                if cmd == 'cd':
+                    subdir = line.split(" ")[2]
+
+                    if subdir == '..':
+                        current_dir = current_dir[:current_dir.rfind('/')]
+
+                    elif subdir == '/':
+                        current_dir = '/'
+
+                    else:
+                        current_dir = current_dir + f'/{subdir}' if current_dir != '/' else f'/{subdir}'
+
+            else:
+                # list file or dirs
+                filetype = line.split(" ")[0]
+
+                if filetype == "dir":
+                    dirname = current_dir + f'/{line.split(" ")[1]}' if current_dir != '/' else f'/{line.split(" ")[1]}'
+                    dir_sizes[dirname] = 0
+                    dir_hierarchy[dirname] = []
+
+                    try:
+                        dir_hierarchy[current_dir].append(dirname)
+
+                    except KeyError:
+                        dir_hierarchy[current_dir] = [dirname]
+
+                else:
+                    # list files
+                    filesize = int(line.split(" ")[0])
+
+                    dir_sizes[current_dir] += filesize
+
+    for parent_dir in dir_hierarchy.keys():
+        child_dirs = dir_hierarchy[parent_dir]
+
+        while child_dirs:
+            child_dir = child_dirs.pop(0)
+            dir_sizes[parent_dir] += dir_sizes[child_dir]
+            child_dirs.extend(dir_hierarchy[child_dir])
+
+    unused_space = 70000000 - dir_sizes['/']
+    min_dir_size = float('inf')
+
+    for dir_size in list(sorted(dir_sizes.values())):
+        if 30000000 <= unused_space + dir_size <= 70000000:
+            print(unused_space)
+            return dir_size
+
+    return min_dir_size
+
+print(solution())
